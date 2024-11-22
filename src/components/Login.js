1aab34a378f81913d0cb2ab8470cafe412b0aa4c
Caleb Ash <calebwilliam@comcast.net>
2024-11-21 23:07:43 -0500

    Testing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,23 @@
 const Login = () => {
-    const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-    const REDIRECT_URI = "https://year-round-wrapped.onrender.com/callback"
-    const SCOPES = "user-top-read";
-  
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(
-      REDIRECT_URI
-    )}&scope=${encodeURIComponent(SCOPES)}`;    
-  
-    return (
-      <div>
-        <h1>Spotify Top Artists App</h1>
-        <a href={authUrl}>
-          <button>Log in with Spotify</button>
-        </a>
-      </div>
-    );
-  };
-  
-  export default Login;
-  
+  const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+  const SCOPES = "user-top-read";
+
+  if (!CLIENT_ID) {
+    console.error("Spotify CLIENT_ID is missing. Please check your .env file.");
+  }
+  console.log(CLIENT_ID);
+
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(
+    REDIRECT_URI
+  )}&scope=${encodeURIComponent(SCOPES)}`;
+
+  return (
+    <div>
+      <h1>Spotify Top Artists App</h1>
+      <button onClick={() => (window.location.href = authUrl)}>Log in with Spotify</button>
+    </div>
+  );
+};
+
+export default Login;
